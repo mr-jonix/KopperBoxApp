1741f94b6b4756b985001eaaa4c9d95ddb7f5356
mr_jonix <submatukana@gmail.com>
2022-05-10 10:22:51 +0300

    First push

---
+++ KopperBoxApp/KopperBoxApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using KopperBoxApp.ViewModels;
+using KopperBoxApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace KopperBoxApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ KopperBoxApp/KopperBoxApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using KopperBoxApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace KopperBoxApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
